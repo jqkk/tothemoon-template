abe7b89a2f6fa541c262d6e12e9795fc62115eb7
Chaeduk <kkj2551@naver.com>
2022-07-11 21:58:13 +0900

    :sparkles: feat: interest 그래프 mock 데이터 연동

--- src/components/InterestChart/index.jsx
+++ src/components/InterestChart/index.jsx
@@ -1,87 +1,87 @@
-// import React, { useEffect } from 'react';
-// import * as am5 from '@amcharts/amcharts5';
-// import * as am5xy from '@amcharts/amcharts/xy';
-// import am5themes_Animated from '@amcharts/amcharts5/themes/Animated';
+import React, { useEffect } from 'react';
+import * as am5 from '@amcharts/amcharts5';
+import * as am5xy from '@amcharts/amcharts5/xy';
+import am5themesAnimated from '@amcharts/amcharts5/themes/Animated';
+import { InterestMock } from '../../mock';
 
-// function InterestChart() {
-//   useEffect(() => {
-//     const root = am5.Root.new('chartdiv');
+function InterestChart() {
+  useEffect(() => {
+    const root = am5.Root.new('chartdiv');
 
-//     root.setThemes([am5themes_Animated.new(root)]);
+    root.setThemes([am5themesAnimated.new(root)]);
 
-//     const chart = root.container.children.push(
-//       am5xy.XYChart.new(root, {
-//         panX: true,
-//         panY: true,
-//         wheelX: 'panX',
-//         wheelY: 'zoomX',
-//         pinchZoomX: true,
-//       }),
-//     );
-//     const cursor = chart.set(
-//       'cursor',
-//       am5xy.XYCursor.new(root, {
-//         behavior: 'none',
-//       }),
-//     );
-//     cursor.lineY.set('visible', false);
+    const chart = root.container.children.push(
+      am5xy.XYChart.new(root, {
+        panX: true,
+        panY: true,
+        wheelX: 'panX',
+        wheelY: 'zoomX',
+        pinchZoomX: true,
+      }),
+    );
+    const cursor = chart.set(
+      'cursor',
+      am5xy.XYCursor.new(root, {
+        behavior: 'none',
+      }),
+    );
+    cursor.lineY.set('visible', false);
 
-//     const xAxis = chart.xAxes.push(
-//       am5xy.DateAxis.new(root, {
-//         maxDeviation: 0.2,
-//         baseInterval: {
-//           timeUnit: 'day',
-//           count: 1,
-//         },
-//         renderer: am5xy.AxisRendererX.new(root, {}),
-//         tooltip: am5.Tooltip.new(root, {}),
-//       }),
-//     );
+    const xAxis = chart.xAxes.push(
+      am5xy.DateAxis.new(root, {
+        maxDeviation: 0.2,
+        baseInterval: {
+          timeUnit: 'day',
+          count: 1,
+        },
+        renderer: am5xy.AxisRendererX.new(root, {}),
+        tooltip: am5.Tooltip.new(root, {}),
+      }),
+    );
 
-//     const yAxis = chart.yAxes.push(
-//       am5xy.ValueAxis.new(root, {
-//         renderer: am5xy.AxisRendererY.new(root, {}),
-//       }),
-//     );
+    const yAxis = chart.yAxes.push(
+      am5xy.ValueAxis.new(root, {
+        renderer: am5xy.AxisRendererY.new(root, {}),
+      }),
+    );
 
-//     const series = chart.series.push(
-//       am5xy.LineSeries.new(root, {
-//         name: 'Series',
-//         xAxis,
-//         yAxis,
-//         valueYField: 'value',
-//         valueXField: 'date',
-//         tooltip: am5.Tooltip.new(root, {
-//           labelText: '댓글 수 : {valueY}',
-//         }),
-//       }),
-//     );
+    const series = chart.series.push(
+      am5xy.LineSeries.new(root, {
+        name: 'Series',
+        xAxis,
+        yAxis,
+        valueYField: 'value',
+        valueXField: 'date',
+        tooltip: am5.Tooltip.new(root, {
+          labelText: '댓글 수 : {valueY}',
+        }),
+      }),
+    );
 
-//     chart.set(
-//       'scrollbarX',
-//       am5.Scrollbar.new(root, {
-//         orientation: 'horizontal',
-//       }),
-//     );
+    chart.set(
+      'scrollbarX',
+      am5.Scrollbar.new(root, {
+        orientation: 'horizontal',
+      }),
+    );
 
-//     (async function () {
-//       const result = await axios.get(`http://localhost:8080/interest/${url}`);
-//       const data = result.data.map(function (cur, index) {
-//         const date = new Date(cur.commentDate);
-//         date.setHours(0, 0, 0, 0);
-//         am5.time.add(date, 'day', 0);
-//         return { date: date.getTime(), value: Number(cur.commentCount) };
-//       });
-//       series.data.setAll(data);
-//       series.appear(1000);
-//       chart.appear(1000, 100);
-//     })();
+    (function setData() {
+      const data = InterestMock.map((cur) => {
+        const date = new Date(cur.date);
+        date.setHours(0, 0, 0, 0);
+        am5.time.add(date, 'day', 0);
+        return { date: date.getTime(), value: cur.count };
+      });
+      series.data.setAll(data);
+      series.appear(1000);
+      chart.appear(1000, 100);
+    })();
 
-//     return () => {
-//       root.dispose();
-//     };
-//   });
-//   return <div id="chartdiv" style={{ width: '80%', height: '700px' }} />;
-// }
+    return () => {
+      root.dispose();
+    };
+  });
+  return <div id="chartdiv" style={{ width: '80%', height: '700px' }} />;
+}
 
-// export default InterestChart;
+export default InterestChart;
---
+++ src/mock/index.js
@@ -0,0 +1,4 @@
+import InterestMock from './interest';
+import VideoInfoMock from './videoInfo';
+
+export { InterestMock, VideoInfoMock };
