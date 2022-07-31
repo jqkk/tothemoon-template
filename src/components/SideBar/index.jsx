291a401d15e40e9dad5e5fe438902019c8eb129b
Chaeduk <kkj2551@naver.com>
2022-07-31 22:15:33 +0900

    :bug: fix: scroll 시, navItem이 바뀌도록 수정

--- src/components/SideBar/index.jsx
+++ src/components/SideBar/index.jsx
@@ -1,14 +1,25 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import * as S from './style';
 
 function SideBar({ elements }) {
   const [state, setState] = useState(0);
+
+  const handleScroll = () => {
+    const vh = window.scrollY * (100 / document.documentElement.clientHeight);
+    setState(parseInt(vh / 100, 10));
+  };
+
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll);
+    return () => {
+      window.addEventListener('scroll', handleScroll);
+    };
+  });
   return (
     <S.NavBar>
       <S.NavContainer>
         {elements.map((element) => {
           const onClick = () => {
-            setState(element.key);
             element.onMoveToElement();
           };
           return (
